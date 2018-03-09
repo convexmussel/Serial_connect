68a44ff4e24c068ed1fe501066b4aa8ced9a54e1
Jarno Voortman <jarno.voortman@gmail.com>
2018-03-09 23:34:17 +0100

    First push everything works basic working

---
+++ serial/mainscreen.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace serial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.to = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.from = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.comselect = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.send = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 72);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.connect_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(292, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(194, 72);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Disconnect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.disconnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "baud rate";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "110",
+            "300",
+            "600",
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "14400",
+            "19200",
+            "38400",
+            "57600",
+            "115200",
+            "128000",
+            "256000"});
+            this.comboBox1.Location = new System.Drawing.Point(39, 151);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // to
+            // 
+            this.to.Location = new System.Drawing.Point(6, 26);
+            this.to.Multiline = true;
+            this.to.Name = "to";
+            this.to.ReadOnly = true;
+            this.to.Size = new System.Drawing.Size(192, 195);
+            this.to.TabIndex = 9;
+            // 
+            // from
+            // 
+            this.from.Location = new System.Drawing.Point(6, 26);
+            this.from.Multiline = true;
+            this.from.Name = "from";
+            this.from.ReadOnly = true;
+            this.from.Size = new System.Drawing.Size(192, 195);
+            this.from.TabIndex = 10;
+            // 
+            // comselect
+            // 
+            this.comselect.FormattingEnabled = true;
+            this.comselect.Location = new System.Drawing.Point(39, 97);
+            this.comselect.Name = "comselect";
+            this.comselect.Size = new System.Drawing.Size(121, 28);
+            this.comselect.TabIndex = 11;
+            this.comselect.SelectedIndexChanged += new System.EventHandler(this.comselect_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Com Port";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.comselect);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(526, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(194, 197);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connection controlls";
+            // 
+            // button3
+            // 
+            this.button3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button3.Location = new System.Drawing.Point(39, 25);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 46);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "0";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // send
+            // 
+            this.send.Location = new System.Drawing.Point(298, 231);
+            this.send.Multiline = true;
+            this.send.Name = "send";
+            this.send.Size = new System.Drawing.Size(188, 195);
+            this.send.TabIndex = 14;
+            this.send.KeyDown += new System.Windows.Forms.KeyEventHandler(this.send_KeyDown);
+            this.send.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.send_KeyPress);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.to);
+            this.groupBox2.Location = new System.Drawing.Point(61, 205);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(204, 227);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "debug";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(292, 205);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(204, 227);
+            this.groupBox3.TabIndex = 16;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "to";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.from);
+            this.groupBox4.Location = new System.Drawing.Point(516, 205);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(204, 227);
+            this.groupBox4.TabIndex = 16;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "from";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.send);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Name = "Form1";
+            this.Text = "Serial Monitor";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox to;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox from;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox comselect;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox send;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+    }
+}
+
---
+++ serial/mainscreen.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace serial
+{
+    public partial class Form1 : Form
+    {
+        public string a = "";
+        public string com = "";
+        public int baudss = 9600;
+        static SerialPort serialport;
+        public Form1()
+        {
+            serialport = new SerialPort();
+            InitializeComponent();
+            setup();
+        }
+
+        private void setup()
+        {
+            comboBox1.SelectedIndex = 6;
+            serialport.ReceivedBytesThreshold = 1; 
+            serialport.NewLine = "\n";               
+            serialport.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(SerialPort1_DataReceived);
+            updateCom();
+            comselect.SelectedIndex = 0;
+        }
+
+        private void updateCom()
+        {
+            comselect.Items.Clear();
+            try
+            {
+                string[] ports = SerialPort.GetPortNames();
+                foreach (String a in ports)
+                {
+                    comselect.Items.Add(a);
+                }
+                comselect.SelectedIndex = 0;
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("cannot access com ports");
+            }
+        }
+
+        private void Send_Data(String send)
+        {
+            if (serialport.IsOpen)
+            {
+                serialport.WriteLine(send);
+            }
+            else
+            {
+                to.AppendText("unable to send no connection port closed" + Environment.NewLine);
+                updateCom();
+            }
+        }
+
+        private void SerialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                a = serialport.ReadLine();
+                Invoke(new EventHandler(update));
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+
+        private void update(object sender, EventArgs e)
+        {
+            from.AppendText(a + Environment.NewLine);
+        }
+
+        private void connect_Click(object sender, EventArgs e)
+        {
+            if (!serialport.IsOpen)
+            {
+                openconnection();
+                if (serialport.IsOpen)
+                {
+                    to.AppendText("connection opened" + Environment.NewLine);
+                }
+                else
+                {
+                    to.AppendText("not opened" + Environment.NewLine);
+                }
+            }
+
+        }
+
+        private void openconnection()
+        {
+            serialport.PortName = com;
+            serialport.BaudRate = baudss;
+            serialport.Open();
+        }
+
+        private void closeconnection()
+        {
+            serialport.Close();
+        }
+
+     
+
+        private void disconnect_Click(object sender, EventArgs e)
+        {
+            if (serialport.IsOpen)
+            {
+                closeconnection();
+                to.AppendText("connection closed" + Environment.NewLine);
+            }
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                baudss = Int32.Parse(comboBox1.Text);
+            }
+            catch (FormatException)
+            {
+                to.AppendText("error");
+            }
+            if (serialport.IsOpen)
+            {
+                closeconnection();
+                openconnection();
+            }
+
+        }
+
+        private void comselect_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            com = comselect.Text;
+            if (serialport.IsOpen)
+            {
+                closeconnection();
+                openconnection();
+            }
+        }
+
+        private void send_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           
+        }
+
+        private void send_KeyDown(object sender, KeyEventArgs e)
+        {
+           
+            if (e.KeyCode == Keys.Enter)
+            {
+                string[] lines = send.Text.Split('\n', '\r');
+                string last_line = lines[lines.Length - 1];
+                Send_Data(last_line);
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            updateCom();
+        }
+
+    }
+}
